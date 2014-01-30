c68f54eebc8553b5eaaaca9a0107cfbf098d2745
Mark Tomm <marktomm90@gmail.com>
2014-01-31 00:30:33 +0200

    Updated compile script

--- CompileProcessManager.sh
+++ CompileProcessManager.sh
@@ -1,1 +1,1 @@
-g++ -Wall ProcessManager.cpp ProcessManagerException.cpp main.cpp -o main -DPM_DEBUG -lboost_system -lboost_filesystem -lboost_date_time -lboost_thread
+g++ -Wall LinuxProcessManager.cpp ProcessManagerException.cpp main.cpp -o main -DPM_DEBUG -lboost_system -lboost_filesystem -lboost_date_time -lboost_thread
